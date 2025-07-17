561b19bf6b21d3e66fccda8a56bc4f8a9479a0f1
JPCalsavara <jpcalsavara@gmail.com>
2025-07-17 15:41:34 -0300

    Arrumando tamanho da tela principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import HeroSection from "@/components/HeroSection";
 export default function Home() {
   return (
     <>
-      <div className="container mx-auto">
+      <div className="w-full">
         <HeroSection></HeroSection>
         <CredibilitySection></CredibilitySection>
         <GaleriaArvores></GaleriaArvores>
